12c64b1da04a3d16669cda274b8b58b40eddbfcc
unknown <bum111997x@gmail.com>
2019-09-04 23:11:34 +0700

    file js của máy tính nâng cao

---
+++ Bai_2_3/plus.js
@@ -0,0 +1,6 @@
+function plus() {
+    let a=parseFloat(document.getElementById('nhap1').value);
+    let b=parseFloat(document.getElementById('nhap2').value);
+    kq=a+b;
+    document.getElementById('ketqua').value = kq;
+}
