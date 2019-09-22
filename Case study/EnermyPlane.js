44ee724ba6c36188b2eec9b0ff80e84749709e44
unknown <bum111997x@gmail.com>
2019-09-22 16:15:29 +0700

    EnemyPlane

---
+++ Case study/EnermyPlane.js
@@ -0,0 +1,27 @@
+let EnemyPlane = function (speed, x, y) {
+    let ctx = document.getElementById('Canvas').getContext("2d");
+    this.speed = speed;
+    this.x = Math.floor(Math.random()*350)+30;
+    this.y = y;
+    this.size =25;
+
+    this.move = function () {
+        return this.y += this.speed;
+    };
+
+    this.getX = function () {
+        return this.x;
+    };
+
+    this.getY = function () {
+        return this.y;
+    };
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+};
+
