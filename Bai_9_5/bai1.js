c1e17bfe16d89752b39da3a2b73a56fe911452c3
unknown <bum111997x@gmail.com>
2019-09-22 22:11:06 +0700

    [Bài tập] Validate số điện thoại

---
+++ Bai_9_5/bai1.js
@@ -0,0 +1,9 @@
+function PhoneNumber() {
+    let regex = /^\([0-9]{2}\)\-\([0][0-9]{9}\)$/
+    if (regex.test(prompt("input phone number"))) {
+        alert("accept");
+    } else {
+        alert("reject!");
+    }
+}
+PhoneNumber();
